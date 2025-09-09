77e268a7b28c042d30588c41e1c840e0ebde6a81
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-09-09 02:35:13 +0000

    Made a simple Java Program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Napoleon Bonaparte!");
+        System.out.println("Here's 1 additional statement!");
+    }
+}
